5ea93cee029c04630b7311869aeda51d4c18c23e
root <root@db05b02.eng.platformlab.ibm.com>
2014-08-08 08:53:47 -0400

    first commit

---
+++ i2psam/mak.bat
@@ -0,0 +1,3 @@
+qmake "USE_QRCODE=1" "USE_UPNP=1" i2psam.pro
+
+mingw32-make.exe /?
---
+++ src/noui.cpp
@@ -0,0 +1,47 @@
+// Copyright (c) 2010 Satoshi Nakamoto
+// Copyright (c) 2009-2012 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include "ui_interface.h"
+#include "init.h"
+#include "bitcoinrpc.h"
+
+#include <string>
+
+static int noui_ThreadSafeMessageBox(const std::string& message, const std::string& caption, int style)
+{
+    printf("%s: %s\n", caption.c_str(), message.c_str());
+    fprintf(stderr, "%s: %s\n", caption.c_str(), message.c_str());
+    return 4;
+}
+
+static void noui_ThreadSafeShowGeneratedI2PAddress(const std::string& caption, const std::string& pub, const std::string& priv, const std::string& b32, const std::string& configFileName)
+{
+    std::string msg = "\nIf you want to use a permanent I2P-address you have to set a \'mydestination\' option in the configuration file:\n";
+    msg += configFileName;
+
+    msg += "\nAddress + private key (save this text in the configuration file and keep it secret):\n";
+    msg += priv;
+
+    msg += "\n\nAddress (you can make it public):\n";
+    msg += pub;
+
+    msg += "\n\nShort base32-address:\n";
+    msg += b32;
+    msg += "\n\n";
+
+    printf("%s: %s\n", caption.c_str(), msg.c_str());
+    fprintf(stderr, "%s: %s\n", caption.c_str(), msg.c_str());
+}
+
+static bool noui_ThreadSafeAskFee(int64_t nFeeRequired, const std::string& strCaption)
+{
+    return true;
+}
+
+void noui_connect()
+{
+    // Connect bitcoind signal handlers
+    uiInterface.ThreadSafeMessageBox.connect(noui_ThreadSafeMessageBox);
+    uiInterface.ThreadSafeAskFee.connect(noui_ThreadSafeAskFee);
+}
